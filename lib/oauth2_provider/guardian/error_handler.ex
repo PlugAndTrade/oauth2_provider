453974e3f47d15af1bf8d88a3761be816a1ec153
Simon Thörnqvist <simon@plug-n-trade.com>
2020-01-28 16:53:57 +0100

    pass provider forward in case of not authenticated

--- lib/oauth2_provider/guardian/error_handler.ex
+++ lib/oauth2_provider/guardian/error_handler.ex
@@ -10,13 +10,20 @@ defmodule Oauth2Provider.Guardian.ErrorHandler do
   end
 
   @impl Guardian.Plug.ErrorHandler
-  def auth_error(conn, {type, reason}, opts) do
+  def auth_error(%Plug.Conn{} = conn, {type, reason}, opts) do
+    params = Map.take(conn.query_params, "provider")
+
+    query =
+      %{"redirect_to" => URI.encode_www_form(request_url(conn))}
+      |> Map.merge(params)
+      |> URI.encode_query()
+
     Logger.error("auth_error #{inspect({type, reason, opts})}")
 
     conn
     |> put_resp_header(
       "location",
-      "#{Confex.fetch_env!(:oauth2_provider, :path_prefix)}/token?redirect_to=#{URI.encode_www_form(request_url(conn))}"
+      "#{Confex.fetch_env!(:oauth2_provider, :path_prefix)}/token?#{query}"
     )
     |> send_resp(302, "")
   end
